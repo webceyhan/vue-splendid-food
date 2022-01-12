2947dbc674c215e610d4c0a8fe0e75d363598a3c
Evren Ceyhan <evren@ceyhan.io>
2022-01-12 18:44:45 +0300

    product card moved into component

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="card bg-dark text-white rounded-5">
+    <div class="card-image-top text-center pt-3">
+      <i :class="`icofont-10x icofont-${product.icon} text-success`"></i>
+    </div>
+
+    <div class="card-body p-4">
+      <h4 class="card-title mb-4">{{ product.name }}</h4>
+
+      <dl class="row mb-4">
+        <hr />
+        <dt class="col-sm-5">Price:</dt>
+        <dd class="col-sm-7">${{ product.price }}</dd>
+        <hr />
+        <dt class="col-sm-5">Type:</dt>
+        <dd class="col-sm-7">{{ product.type }}</dd>
+      </dl>
+
+      <div class="input-group input-group-sm">
+        <input type="number" class="form-control" value="1" />
+        <button
+          class="
+            btn btn-primary
+            d-flex
+            align-items-center
+            justify-content-center
+            flex-fill
+          "
+        >
+          Add
+          <i class="icofont-cart-alt icofont-2x ms-2"></i>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props:['product']
+};
+</script>
+
+<style>
+</style>
--- src/views/Products.vue
+++ src/views/Products.vue
@@ -5,40 +5,7 @@
 
       <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-4">
         <div class="col" v-for="food in foods" :key="food.id">
-          <div class="card bg-dark text-white rounded-5">
-            <div class="card-image-top text-center pt-3">
-              <i :class="`icofont-10x icofont-${food.icon} text-success`"></i>
-            </div>
-
-            <div class="card-body p-4">
-              <h4 class="card-title mb-4">{{ food.name }}</h4>
-
-              <dl class="row mb-4">
-                <hr />
-                <dt class="col-sm-5">Price:</dt>
-                <dd class="col-sm-7">${{ food.price }}</dd>
-                <hr />
-                <dt class="col-sm-5">Type:</dt>
-                <dd class="col-sm-7">{{ food.type }}</dd>
-              </dl>
-
-              <div class="input-group input-group-sm">
-                <input type="number" class="form-control" value="1" />
-                <button
-                  class="
-                    btn btn-primary
-                    d-flex
-                    align-items-center
-                    justify-content-center
-                    flex-fill
-                  "
-                >
-                  Add
-                  <i class="icofont-cart-alt icofont-2x ms-2"></i>
-                </button>
-              </div>
-            </div>
-          </div>
+          <ProductCard :product="food" />
         </div>
       </div>
     </div>
@@ -47,8 +14,10 @@
 
 <script>
 import store from "@/store";
+import ProductCard from "@/components/ProductCard";
 
 export default {
+  components: { ProductCard },
   computed: {
     foods() {
       return store.foods;
