aab7d527b7eed4ea0ab7bee55e83096e742cc57c
Evren Ceyhan <evren@ceyhan.io>
2022-01-12 20:19:51 +0300

    sample cart data added

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,9 @@
 import foods from './data/foods.json';
 import orders from './data/orders.json';
+import cart from './data/cart.json';
 
 export default {
     foods,
-    orders
+    orders,
+    cart,
 };
