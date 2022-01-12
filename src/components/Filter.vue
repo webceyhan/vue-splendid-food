190f797a80602ca724883cdda977c9512f8259fc
Evren Ceyhan <evren@ceyhan.io>
2022-01-13 00:15:09 +0300

    product-filter moved to component

---
+++ src/components/Filter.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="btn-group" role="group">
+    <template v-for="(option, i) in options" :key="i">
+      <input
+        type="radio"
+        class="btn-check"
+        name="filter"
+        :id="`filter-${i}`"
+        :value="option"
+        :checked="option === modelValue"
+        @change="$emit('update:modelValue', option)"
+        autocomplete="off"
+      />
+      <label :for="`filter-${i}`" class="btn btn-outline-primary">
+        {{ option }}
+      </label>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ["update:modelValue"],
+  props: ["modelValue", "options"],
+  computed: {
+    size() {
+      return this.sm ? 2 : this.lg ? 10 : 4;
+    },
+    classes() {
+      return `icofont-${this.name} icofont-${this.size}x`;
+    },
+  },
+};
+</script>
--- src/views/Products.vue
+++ src/views/Products.vue
@@ -3,27 +3,7 @@
     <div class="container py-5">
       <header class="d-flex align-items-center justify-content-between mb-3">
         <h1 class="mb-0">Products</h1>
-
-        <div
-          class="btn-group"
-          role="group"
-          aria-label="Basic radio toggle button group"
-        >
-          <template v-for="(type, i) in productTypes" :key="i">
-            <input
-              :id="`btnradio-${i}`"
-              type="radio"
-              class="btn-check"
-              name="btnradio"
-              :value="type"
-              v-model="productType"
-              autocomplete="off"
-            />
-            <label :for="`btnradio-${i}`" class="btn btn-outline-primary">{{
-              type
-            }}</label>
-          </template>
-        </div>
+        <Filter :options="productTypes" v-model="productType" />
       </header>
 
       <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-4">
@@ -37,10 +17,11 @@
 
 <script>
 import store from "@/store";
+import Filter from "@/components/Filter";
 import ProductCard from "@/components/ProductCard";
 
 export default {
-  components: { ProductCard },
+  components: { Filter, ProductCard },
   data() {
     return { productType: "all" };
   },
